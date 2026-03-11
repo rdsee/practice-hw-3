00414fb236109ce9b313709d52b611ef93838500
rdsee <rost.slobod@gmail.com>
2026-03-11 12:16:44 +0200

    exeption handling added

--- practice.py
+++ practice.py
@@ -1,4 +1,3 @@
-print("hell oowdld")
 
 print ('1. first \n2. second \n3. third \n4. fourth')
 user_select = int(input("enter your choice: "))
@@ -14,4 +13,25 @@ match user_select:
         print ('4')
     case _:
         print('error')
+
+print('\t')
+
+try:
+    num1 = int(input(print('Enter the first number: ')))
+    num2 = int(input(print('Enter the second number: ')))
+
+    result = num1 / num2
+    print(f'result: {result}')
+
+except ZeroDivisionError as error_1:
+    print('You cannot divide by zero')
+    print(f'Error: {error_1}')
+except ValueError as error_2:
+    print('You need to enter a number')
+    print(f'Error: {error_2}')
+except Exception as error_3:
+    print('Exception occured!')
+    print(f'Error: {error_3}')
+finally:
+    print('End of calculation')
 
